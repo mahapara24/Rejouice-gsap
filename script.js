b36dc4cca471e4aba06fbc78d9be95f11eba5b4a
Mahapara <mahapara2424@gmail.com>
2024-11-21 10:55:55 +0500

    updated the code addition added

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 var page1Content = document.querySelector("#page1-content");
 var Cursor = document.querySelector("#cursor");
 
-page1Content.addEventListener("mousemove", function () {});
+page1Content.addEventListener("mousemove", function (e) {
+  gsap.to(Cursor, { x: e.x, y: e.y });
+});
+
+page1Content.addEventListener("mouseenter", function () {
+  gsap.to(Cursor, {
+    scale: 1,
+  });
+});
+
+page1Content.addEventListener("mouseleave", function () {
+  gsap.to(Cursor, {
+    scale: 0,
+  });
+});
