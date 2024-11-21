16c58096c225ac6f94060b8eed3cef137bca82e1
Mahapara <mahapara2424@gmail.com>
2024-11-21 10:58:49 +0500

    updated the code addition added

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-var page1Content = document.querySelector("#page1-content");
-var Cursor = document.querySelector("#cursor");
+function cursorEffect() {
+  var page1Content = document.querySelector("#page1-content");
+  var Cursor = document.querySelector("#cursor");
 
-page1Content.addEventListener("mousemove", function (e) {
-  gsap.to(Cursor, { x: e.x, y: e.y });
-});
-
-page1Content.addEventListener("mouseenter", function () {
-  gsap.to(Cursor, {
-    scale: 1,
+  page1Content.addEventListener("mousemove", function (e) {
+    gsap.to(Cursor, { x: e.x, y: e.y });
   });
-});
 
-page1Content.addEventListener("mouseleave", function () {
-  gsap.to(Cursor, {
-    scale: 0,
+  page1Content.addEventListener("mouseenter", function () {
+    gsap.to(Cursor, {
+      scale: 1,
+      opacity: 1,
+    });
   });
-});
+
+  page1Content.addEventListener("mouseleave", function () {
+    gsap.to(Cursor, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+}
+cursorEffect();
